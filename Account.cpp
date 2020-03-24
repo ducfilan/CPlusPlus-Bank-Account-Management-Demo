11759a6301160f0f823a4ef39a1be3a670fcafd9
Duc Filan <ducfilan@gmail.com>
2020-03-24 13:32:03 +0900

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -13,39 +13,42 @@ Account::Account()
 //Dtor
 Account::~Account()
 {
-	if(name!=NULL)
-	{
-		delete[] name;
-		name=NULL;
-	}
-	if(userID!=NULL)
-	{
-		delete[] userID;
-		userID=NULL;
-	}
-		
+    if(name!=NULL)
+    {
+        delete[] name;
+        name=NULL;
+    }
+    if(userID!=NULL)
+    {
+        delete[] userID;
+        userID=NULL;
+    }
+        
 }
+
 //Set
 void Account::set(char AuserID[],string Apass,char Aname[],double Abalance)
 {
-	strcpy(userID,AuserID);
-	strcpy(name,Aname);
-	pass=Apass;
-	balance=Abalance;
+    strcpy(userID,AuserID);
+    strcpy(name,Aname);
+    pass=Apass;
+    balance=Abalance;
 }
+
 //Ctor 3 para.s
 Account::Account(char AuserID[],string Apass,char Aname[],double Abalance)
 {
-	name=new char[strlen(name)+1];
-	userID=new char[strlen(AuserID)+1];
+    name=new char[strlen(name)+1];
+    userID=new char[strlen(AuserID)+1];
     set(AuserID,Apass,Aname,Abalance);
 }
+
 //MAIN MENU
 void mainMenu(Account** a)
 {
     
-	int n=0;
-	char choice;
+    int n=0;
+    char choice;
     while(1)
     {
         system("cls");
@@ -53,7 +56,7 @@ void mainMenu(Account** a)
         cout<<"|         BANK ACCOUNT MANAGEMENT        |"<<endl;
         cout<<"+========================================+"<<endl;
         cout<<"| 1. Create default 2 Saving accounts    |"<<endl;
-		cout<<"|    and 2 Checking account              |"<<endl;
+        cout<<"|    and 2 Checking account              |"<<endl;
         cout<<"| 2. Create Account                      |"<<endl;
         cout<<"| 3. Login                               |"<<endl;
         cout<<"| 4. View All                            |"<<endl;
@@ -62,29 +65,29 @@ void mainMenu(Account** a)
         cout<<"+----------------------------------------+"<<endl;
         cout<<"| Your seclection(0->3): ";
         fflush(stdin);
-		cin>>choice;
+        cin>>choice;
         if(choice=='0') exit(0);
         switch(choice)
         {
-		case '1':
-			createDefault(a,n);
-			break;
+        case '1':
+            createDefault(a,n);
+            break;
         case '2':
             createAnAccount(a,n);
             break;
         case '3':
-			login(a,n);
+            login(a,n);
             break;
-		case '4':
-			viewAll(a,n);
-			break;
-		case '5':
-	        saveDataToFile(a,"Account.txt",n);
-	        cout<<"| Successfully saved to Accounts.txt!"<<endl<<endl;
-	        break;
+        case '4':
+            viewAll(a,n);
+            break;
+        case '5':
+            saveDataToFile(a,"Account.txt",n);
+            cout<<"| Successfully saved to Accounts.txt!"<<endl<<endl;
+            break;
         default:
             cout<<"| Your selection must(0->2). Try again! "<<endl;
-	        break;
+            break;
         }
         system("pause");
     }
@@ -93,99 +96,102 @@ void mainMenu(Account** a)
 //Create default
 void createDefault(Account** a,int& n)
 {
-	if(n==0)
-	{
-		a[n]=new SavingAccount;
-			a[n++]->set("ducfilan","123456","Hoang Nghia Duc",9999);
-		a[n]=new SavingAccount;
-			a[n++]->set("clever_sheep","123456","Tran Trong Tien Dat",1000);
-		a[n]=new CheckingAccount;
-			a[n++]->set("anhcam_yb","123456","Vu Manh Cam",2000);
-		a[n]=new CheckingAccount;
-			a[n++]->set("l.daBest","123456","Nguyen Tung Lam",2500);
-	}
-	
-	cout<<"|"<<endl<<"| Successfully created Accounts!"<<endl<<endl;
+    if(n == 0)
+    {
+        a[n]=new SavingAccount;
+            a[n++]->set("ducfilan","123456","Hoang Nghia Duc",9999);
+        a[n]=new SavingAccount;
+            a[n++]->set("clever_sheep","123456","Tran Trong Tien Dat",1000);
+        a[n]=new CheckingAccount;
+            a[n++]->set("anhcam_yb","123456","Vu Manh Cam",2000);
+        a[n]=new CheckingAccount;
+            a[n++]->set("l.daBest","123456","Nguyen Tung Lam",2500);
+    }
+    
+    cout<<"|"<<endl<<"| Successfully created Accounts!"<<endl<<endl;
 }
+
 //Accept Account
 char* acceptID()
 {
-	char* value;
-	value=new char[50];
-	do
+    char* value;
+    value=new char[50];
+    do
     {
         cout<<"| Enter your user ID  : ";
         fflush(stdin);
         cin.getline(value,50);
         if(strlen(value)>15 || strlen(value)<1)
         {
-        	cout<<"| UserID's length is unaccepted. Try again!"<<endl;
-			continue;
+            cout<<"| UserID's length is unaccepted. Try again!"<<endl;
+            continue;
         }
         return value;
-	}while(1);
+    }while(1);
 }
 //Accept Password
 string acceptPW(char* x)
 {
-	string Apass;
-	do
-	{
-		cout<<x;
-		Apass=inputPassword(50);
-		if(Apass.size()<6)
-		{
-			cout<<"| Password must contain at least 6 chars!"<<endl;
-			continue;
-		}
-		return Apass;
-	}while(1);
+    string Apass;
+    do
+    {
+        cout<<x;
+        Apass=inputPassword(50);
+        if(Apass.size()<6)
+        {
+            cout<<"| Password must contain at least 6 chars!"<<endl;
+            continue;
+        }
+        return Apass;
+    } while(1);
 }
+
 //Accept Pos
 double acceptPos(char* x)
 {
-	double bTemp;
-	bool Ok=false;
-	do
-	{
-		cout<<x;
-		cin>>bTemp;
+    double bTemp;
+    bool Ok=false;
+    do
+    {
+        cout<<x;
+        cin>>bTemp;
 
-		if(cin.fail())
-		{
-			cerr<<"| Invalid number! Try again!"<<endl;
-			cin.clear();
-			fflush(stdin);
-		}
-		else if(char(cin.get())!='\n')
-		{
-			cout<<"| Trailing characters! Try Again!"<<endl;
-			fflush(stdin);
-		}
-		else if(bTemp<0)
-		{
-			cout<<"| The value must be positive!"<<endl;
-			fflush(stdin);
-		}else
-			Ok=true;
-	}while(!Ok);
-	return bTemp;
+        if(cin.fail())
+        {
+            cerr<<"| Invalid number! Try again!"<<endl;
+            cin.clear();
+            fflush(stdin);
+        }
+        else if(char(cin.get())!='\n')
+        {
+            cout<<"| Trailing characters! Try Again!"<<endl;
+            fflush(stdin);
+        }
+        else if(bTemp<0)
+        {
+            cout<<"| The value must be positive!"<<endl;
+            fflush(stdin);
+        }else
+            Ok=true;
+    }while(!Ok);
+    return bTemp;
 }
+
 //Create Accs
 void createAnAccount(Account** a,int& n)
 {
-	system("cls");
+    system("cls");
     cout<<"+========================================+"<<endl;
     cout<<"|            [Account Register]          |"<<endl;
     cout<<"+========================================+"<<endl;
     char* AuserID,*Aname;
-	string Apass,ApassConfirm;
+    string Apass,ApassConfirm;
     double Abalance;
     char choice;
-	bool isOk=false;
-	
+    bool isOk=false;
+    
     //Enter account name
-	Aname=new char[50];
+    Aname=new char[50];
     do
     {
         cout<<"| Enter your name     : ";
@@ -193,78 +199,79 @@ void createAnAccount(Account** a,int& n)
         cin.getline(Aname,50);
         if (strlen(Aname)>30 || strlen(Aname)<2)
         {
-        	cout<<"| Account length is unaccepted! Try again!"<<endl;
-        	continue;
-		}
+            cout<<"| Account length is unaccepted! Try again!"<<endl;
+            continue;
+        }
         for(int i=0;i<strlen(Aname);i++)
-        	if(Aname[i]<' ' || (Aname[i]>' ' && Aname[i]<'A') || Aname[i]>'z')
-        	{
-				isOk=false;
-				cout<<"| Invalid name format. Try again!"<<endl;
-				break;
-			}else isOk=true;
+            if(Aname[i]<' ' || (Aname[i]>' ' && Aname[i]<'A') || Aname[i]>'z')
+            {
+                isOk=false;
+                cout<<"| Invalid name format. Try again!"<<endl;
+                break;
+            }else isOk=true;
     }while(!isOk);
-	
+    
     //Enter account ID
     AuserID=new char[16];
     do
     {
         AuserID=acceptID();
-    	//Check userID exist
+        //Check userID exist
     
-	    int k=0;
-	    for(int i=0;i<n;i++)
-	        if(strcmp(a[i]->getUserID(),AuserID)==0)
-	            k++;
-	    if(k!=0)
-	        cout<<"|\n| Account ID exist! Please try again!\n"<<endl;
-		else break;
+        int k=0;
+        for(int i=0;i<n;i++)
+            if(strcmp(a[i]->getUserID(),AuserID)==0)
+                k++;
+        if(k!=0)
+            cout<<"|\n| Account ID exist! Please try again!\n"<<endl;
+        else break;
     }while(1);
-	//Enter password
-	isOk=false;
-	do
-	{
-		Apass=acceptPW("| Enter Password      : ");
-		ApassConfirm=acceptPW("| Confirm Password    : ");
-		if(Apass==ApassConfirm) isOk=true;
-		else cout<<"| Confirmation is not match!"<<endl;
-	}while(!isOk);
+    //Enter password
+    isOk=false;
+    do
+    {
+        Apass=acceptPW("| Enter Password      : ");
+        ApassConfirm=acceptPW("| Confirm Password    : ");
+        if(Apass==ApassConfirm) isOk=true;
+        else cout<<"| Confirmation is not match!"<<endl;
+    }while(!isOk);
     //Enter account balance
     Abalance=acceptPos("| Enter your balance  : ");
 
     //Enter account type
     cout<<"|\n"
-    	<<"+========================================+\n"
-    	<<"|        Choose your account type        |\n"
-    	<<"+========================================+\n"
-    	<<"| 1. Savings Account                     |\n"
+        <<"+========================================+\n"
+        <<"|        Choose your account type        |\n"
+        <<"+========================================+\n"
+        <<"| 1. Savings Account                     |\n"
         <<"| 2. Checking Account                    |\n"
-	    <<"+----------------------------------------+\n";
-	   
-	do
-	{
-		cout<<"| Your seclection (0->2): ";
-		fflush(stdin);
-	    cin>>choice;
-	    	 if(choice=='1') a[n++]=new SavingAccount(AuserID,Apass,Aname,Abalance,0,0,0);
-	    else if(choice=='2') a[n++]=new CheckingAccount(AuserID,Apass,Aname,Abalance,0,0);
-	    else
-		{
-			cout<<"| Invalid choice!"<<endl;
-	    	continue;
-		}
-	    cout<<"| Successful to create your account!"<<endl<<endl;
-	    break;
-	}while(1);
+        <<"+----------------------------------------+\n";
+       
+    do
+    {
+        cout<<"| Your seclection (0->2): ";
+        fflush(stdin);
+        cin>>choice;
+             if(choice=='1') a[n++]=new SavingAccount(AuserID,Apass,Aname,Abalance,0,0,0);
+        else if(choice=='2') a[n++]=new CheckingAccount(AuserID,Apass,Aname,Abalance,0,0);
+        else
+        {
+            cout<<"| Invalid choice!"<<endl;
+            continue;
+        }
+        cout<<"| Successful to create your account!"<<endl<<endl;
+        break;
+    }while(1);
 }
+
 //LOGIN
 void login(Account** a,int& n)
 {
-	int i,choice,flag=0;
-	char* acc;
-	string pw;
-	
-	system("cls");
+    int i,choice,flag=0;
+    char* acc;
+    string pw;
+    
+    system("cls");
     cout<<"+========================================+"<<endl;
     cout<<"|                  [LOGIN]               |"<<endl;
     cout<<"+========================================+"<<endl;
@@ -275,25 +282,26 @@ void login(Account** a,int& n)
     {
         if(strcmp(acc,a[i]->getUserID())==0 && (pw==a[i]->getPass()))
         {
-			flag=1;
+            flag=1;
             cout<<"| Login Successfully!"<<endl<<endl;
-			system("pause");
-			break;
-		}
-	}
+            system("pause");
+            break;
+        }
+    }
     if(flag==0)
     {
         cout<<"| Sorry. Invalid username or password!"<<endl<<endl;
         return;
-	}
-	a[i]->showMenu();
+    }
+    a[i]->showMenu();
 }
+
 //VIEW BALANCE
 void Account::viewBalance()
 {
-	cout<<"|"<<endl;
-	cout<<"| Your name          : "<<getName()<<endl;
-	cout<<"| User ID            : "<<getUserID()<<endl;
+    cout<<"|"<<endl;
+    cout<<"| Your name          : "<<getName()<<endl;
+    cout<<"| User ID            : "<<getUserID()<<endl;
     cout<<"| Account balance    : "<<getBalance()<<endl;
 }
 //SAVE TO FILE
@@ -302,36 +310,37 @@ void saveDataToFile(Account** a,const char *filename,int& n)
     ofstream os(filename);
     
     os<<"+-----------------------------------------------------------------------------+\n";
-	os<<"|      ID       |              NAME              |   BALANCE  |      TYPE     |\n";
-	os<<"+-----------------------------------------------------------------------------+\n";
-	for(int i=0;i<n;i++)
-	{
-		os<<"|"<<setw(15)<<left<<a[i]->getUserID()<<"|  "<<setw(30)<<left
-		<<a[i]->getName()<<"|  "<<setw(10)<<left<<a[i]->getBalance()<<"|  ";
+    os<<"|      ID       |              NAME              |   BALANCE  |      TYPE     |\n";
+    os<<"+-----------------------------------------------------------------------------+\n";
+    for(int i=0;i<n;i++)
+    {
+        os<<"|"<<setw(15)<<left<<a[i]->getUserID()<<"|  "<<setw(30)<<left
+        <<a[i]->getName()<<"|  "<<setw(10)<<left<<a[i]->getBalance()<<"|  ";
         if(a[i]->getType()==1)
-        	os<<setw(14)<<right<<"Saving |"<<endl;
+            os<<setw(14)<<right<<"Saving |"<<endl;
         else os<<setw(14)<<right<<"Checking |"<<endl;
-	}
-	os<<"+-----------------------------------------------------------------------------+\n";
+    }
+    os<<"+-----------------------------------------------------------------------------+\n";
     os.close();
 }
 
 void viewAll(Account** a,int& n)
 {
-	system("cls");
-	cout<<"+-----------------------------------------------------------------------------+\n";
-	cout<<"|      ID       |              NAME              |   BALANCE  |      TYPE     |\n";
-	cout<<"+-----------------------------------------------------------------------------+\n";
-	for(int i=0;i<n;i++)
-	{
-		cout<<"|"<<setw(15)<<left<<a[i]->getUserID()<<"|  "<<setw(30)<<left
-		<<a[i]->getName()<<"|  "<<setw(10)<<left<<a[i]->getBalance()<<"|  ";
+    system("cls");
+    cout<<"+-----------------------------------------------------------------------------+\n";
+    cout<<"|      ID       |              NAME              |   BALANCE  |      TYPE     |\n";
+    cout<<"+-----------------------------------------------------------------------------+\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<"|"<<setw(15)<<left<<a[i]->getUserID()<<"|  "<<setw(30)<<left
+        <<a[i]->getName()<<"|  "<<setw(10)<<left<<a[i]->getBalance()<<"|  ";
         if(a[i]->getType()==1)
-        	cout<<setw(14)<<right<<"Saving |"<<endl;
+            cout<<setw(14)<<right<<"Saving |"<<endl;
         else cout<<setw(14)<<right<<"Checking |"<<endl;
-	}
-	cout<<"+-----------------------------------------------------------------------------+\n";
+    }
+    cout<<"+-----------------------------------------------------------------------------+\n";
 }
+
 //PASSWORD - CODE THAM KHAO
 string inputPassword(size_t length_max)
 {
@@ -347,28 +356,32 @@ string inputPassword(size_t length_max)
             pass.push_back(ch);
         }
         else if('\b' == ch && !pass.empty())
-            {
-                cout << "\b \b";
-                pass.resize(pass.size() - 1);
-            }
-    }while ('\r' != ch);
+        {
+            cout << "\b \b";
+            pass.resize(pass.size() - 1);
+        }
+    } while ('\r' != ch);
     cout << endl;
     return pass;
 }
+
 //Get
 char* Account::getName()
 {
-	return name;
-} 
+    return name;
+}
+
 char* Account::getUserID()
 {
-	return userID;
+    return userID;
 }
+
 string Account::getPass()
 {
-	return pass;
+    return pass;
 }
+
 double Account::getBalance()
 {
-	return balance;
+    return balance;
 }
